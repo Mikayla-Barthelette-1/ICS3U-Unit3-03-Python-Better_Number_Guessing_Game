a1fba7411ff570551e4e94a9e2ac5c146d104c0f
Ubuntu <ubuntu@ip-172-31-81-184.ec2.internal>
2021-09-21 23:28:08 +0000

    added better_number_guessing_game.py

---
+++ better_number_guessing_game.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# Created by: Mikayla Barthelette
+# Created on: Sept 2021
+# This program creates a better number guessing game
+
+import random
+
+
+def main():
+    # this function creates the game
+
+    # input
+    number_guessed = int(input("Enter the number between 0 - 9: "))
+    random_number = random.randint(0, 9)  # a number between 0 and 9
+
+    # process
+    if number_guessed == random_number:
+        print("You guessed correct!")
+    else:
+        print("Incorrect, the number was {0}".format(random_number))
+
+    # output
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
